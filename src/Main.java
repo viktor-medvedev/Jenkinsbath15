c9944eb38ee0d11efad72804bea2c7b0afc86320
viktor-medvedev <viktormedvedev2022@gmail.com>
2023-05-30 20:19:47 -0500

    Minor change

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
        // System.out.println("Hello world!");
-      for (int i = 0; i<10;i++){
+      for (int i = 0; i<8   ;i++){
           System.out.println("Hello world!");
           Thread.sleep(2000);
 
